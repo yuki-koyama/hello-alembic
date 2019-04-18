4733423374855628d0e384e2fc1ecfa33bcdb12f
Yuki Koyama <yuki@koyama.xyz>
2019-04-18 23:25:00 +0900

    Add a minimum code to generate an alembic file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include <string>
+#include <Alembic/Abc/All.h>
+#include <Alembic/AbcCoreOgawa/All.h>
+
+using Alembic::Abc::OArchive;
+using Alembic::Abc::OObject;
+
 int main()
 {
+    const std::string file_path = "test.abc";
+
+    OArchive o_archive(Alembic::AbcCoreOgawa::WriteArchive(), file_path);
+
     return 0;
 }
