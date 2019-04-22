cf45e9b7e859ce4185b3e8948a040542edeb132e
Yuki Koyama <yuki@koyama.xyz>
2019-04-22 09:36:02 +0900

    Use TimeSampling

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main()
         + 2.0f, + 1.0f, + 1.0f,
     };
     constexpr size_t num_indices = 24;
-    constexpr int indices[] =
+    constexpr int32_t indices[] =
     {
         0, 4, 6, 2,
         5, 1, 3, 7,
@@ -51,12 +51,18 @@ int main()
         4, 5, 7, 6,
     };
     constexpr size_t num_counts = 6;
-    constexpr int counts[] = { 4, 4, 4, 4, 4, 4 };
+    constexpr int32_t counts[] = { 4, 4, 4, 4, 4, 4 };
+    constexpr float dt = 1.0f / 60.0f;
 
     OArchive archive(Alembic::AbcCoreOgawa::WriteArchive(), "test.abc");
 
+    const TimeSampling time_sampling(dt, 0);
+    const uint32_t time_sampling_index = archive.addTimeSampling(time_sampling);
+
     OPolyMesh mesh_obj(OObject(archive, kTop), "mesh");
     OPolyMeshSchema& mesh = mesh_obj.getSchema();
+
+    mesh.setTimeSampling(time_sampling_index);
 
     const OPolyMeshSchema::Sample mesh_samp_1(V3fArraySample((const V3f*) verts_1, num_verts),
                                               Int32ArraySample(indices, num_indices),
